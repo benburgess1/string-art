c8f6ddd9975705f2a1c530750d444a6c5d37772c
Ben Burgess <ben.burgess@hotmail.co.uk>
2025-11-20 23:53:24 +0000

    Restructured Image class - removed Pixel class

--- Process_Image.py
+++ Process_Image.py
@@ -1,6 +1,7 @@
 from PIL import Image as PIL_Image
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 def import_image(filename):
     # Import and convert to grayscale
@@ -25,45 +26,57 @@ class Pixel:
 
 class Image:
     # Image class - essentially a list of pixels, their positions, and their greyscale intensities
-    def __init__(self, img):
-        self.img = img
-        self.pixels = []
-        self.circular_truncate()
-        
-    def circular_truncate(self):
-        if self.img.shape[0] % 2 == 0:
-            self.img = self.img[:-1,:]
-        if self.img.shape[1] % 2 == 0:
-            self.img = self.img[:,:-1]
-        rad = (min(*self.img.shape) - 1) / 2
-        for i in range(self.img.shape[0]):
-            for j in range(self.img.shape[1]):
-                if (i - rad)**2 + (j-rad)**2 <= rad**2:
-                    self.pixels.append(Pixel(r=np.array([(j-rad)/rad,(rad-i)/rad]), z=self.img[i,j]))
+    # Loads a generic image using the PIL library
+    # Then downsizes into a square array of N x N
+    # Marks pixels according to inside vs outside, but keeps the image as a square for 
+    # convenience with manipulating square arrays later
+    # Don't need a Pixel class?
+    def __init__(self, filename, downsize_pixels=101, L_pixels=51, x_offset=0, y_offset=0):
+        self.downsize_pixels = downsize_pixels
+        self.L_pixels = L_pixels
+        self.rad = (L_pixels - 1) / 2
+        self.x_offset = x_offset
+        self.y_offset = y_offset
+        self.raw_img = PIL_Image.open(filename).convert('L')
+        self.raw_bitmap = np.array(self.raw_img)
+        self.img = self.proportional_downsize()
+        self.bitmap = np.array(self.img)
+        self.square_truncate()
+        self.x, self.y = self.calc_positions()
+        self.dx = self.x[0,1] - self.x[0,0]
+        self.dy = self.dx
+        self.inside_mask = self.calc_inside_mask()
 
+    def proportional_downsize(self):
+        # Assumes portrait picture; will add functionality for landscape later
+        [Ly, Lx] = self.raw_bitmap.shape
+        return self.raw_img.resize((self.downsize_pixels,int(Ly*self.downsize_pixels/Lx)), PIL_Image.BICUBIC)
 
+    def square_truncate(self):
+        Ly = self.bitmap.shape[0]
+        i_min = int(Ly/2 - self.L_pixels/2 - self.y_offset)
+        Lx = self.bitmap.shape[1]
+        j_min = int(Lx/2 - self.L_pixels/2 + self.x_offset)
+        self.bitmap = self.bitmap[i_min:i_min+self.L_pixels, j_min:j_min+self.L_pixels]
 
+    def calc_positions(self):
+        x = (np.arange(self.L_pixels) - self.rad) / self.rad
+        y = np.copy(x)
+        xx, yy = np.meshgrid(x, y, indexing='xy')
+        yy = yy[::-1,:]
+        return xx, yy
+    
+    def calc_inside_mask(self):
+        return self.x**2 + self.y**2 < 1
 
+    def preview(self):
+        fig, ax = plt.subplots()
+        ax.imshow(self.bitmap, cmap='gray')
+        circ = patches.Circle(xy=(self.rad, self.rad), radius=self.rad, ec='r', fc=(0,0,0,0), lw=3)
+        ax.add_patch(circ)
+        plt.show()
 
 
 if __name__ == '__main__': 
-    img = PIL_Image.open('Borat.png').convert('L')
-    bitmap = np.array(img)
-
-    img_small = img.resize((128,128), PIL_Image.BICUBIC)
-    bitmap_small = np.array(img_small)
-
-    print(bitmap.shape)
-    print(bitmap.size)
-    print(bitmap_small.shape)
-    print(bitmap_small.size)
-
-    fig, axs = plt.subplots(1,2)
-    axs[0].imshow(img, cmap='gray')
-    axs[1].imshow(bitmap_small, cmap='gray')
-    axs[0].set_title('Original')
-    axs[1].set_title('Downsized')
-    # for ax in axs:
-    #     ax.set_xticks([])
-    #     ax.set_yticks([])
-    plt.show()
+    image = Image('Borat.png', downsize_pixels=111, L_pixels=101, y_offset=-2, x_offset=7)
+    image.preview()
